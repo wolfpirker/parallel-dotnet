b5f768adde3e1128aec85a83a295e82fb9f6796f
Wolfgang Pirker <w_pirker@gmx.de>
2023-01-29 00:45:01 +0100

    cancelling tasks and waiting for time to pass

---
+++ parallel-dotnet4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo1
+{
+    internal static class Program
+    {     
+
+        public static void Main()
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            token.Register(() => 
+            {
+                Console.WriteLine("Cancelation has been requested");
+            });
+
+            var t = new Task(() => {
+                int i = 0;
+                while (true){
+                    token.ThrowIfCancellationRequested();
+                    /* 
+                    // this is the longer way of doing it...
+                    if (token.IsCancellationRequested)
+                    {
+                        throw new OperationCanceledException();
+                    }
+                    */
+                    Console.WriteLine($"{i++}\t");
+                }
+            
+            }, token);
+
+            t.Start();
+
+            // another way of cancellation:
+            Task.Factory.StartNew(() => {
+                token.WaitHandle.WaitOne();
+                Console.WriteLine("CancellationToken has been requested.");
+            });
+
+            Console.ReadKey();
+            cts.Cancel();
+
+            Console.WriteLine("Main program done.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ parallel-dotnet5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntroducingTasks
+{
+  class CancelingTasks
+  {
+    static void Main(string[] args)
+    {
+      CompositeCancelationToken();
+
+      Console.WriteLine("Main program done, press any key.");
+      Console.ReadKey();
+    }    
+
+    private static void CompositeCancelationToken()
+    {
+      // it's possible to create a 'composite' cancelation source that involves several tokens
+      var planned = new CancellationTokenSource();
+      var preventative = new CancellationTokenSource();
+      var emergency = new CancellationTokenSource();
+
+      // make a token source that is linked on their tokens
+      var paranoid = CancellationTokenSource.CreateLinkedTokenSource(
+        planned.Token, preventative.Token, emergency.Token);
+
+      Task.Factory.StartNew(() =>
+      {
+        int i = 0;
+        while (true)
+        {
+          paranoid.Token.ThrowIfCancellationRequested();
+          Console.Write($"{i++}\t");
+          Thread.Sleep(100);
+        }
+      }, paranoid.Token);
+
+      paranoid.Token.Register(() => Console.WriteLine("Cancelation requested"));
+
+      Console.ReadKey();
+
+      // use any of the aforementioned token soures
+      emergency.Cancel();
+    }
+}
+}
