4efb236784fc62c1be14acb70529a8ac25057d95
Wolfgang Pirker <w_pirker@gmx.de>
2023-03-20 22:51:29 +0100

    Parallel dotnet, Asynchronous programming, pt. 1

---
+++ AsyncAwait/MainFrame.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AsyncAwait
+{
+  partial class MainFrame
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.BtnCalculate = new System.Windows.Forms.Button();
+      this.LblResult = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // BtnCalculate
+      // 
+      this.BtnCalculate.Location = new System.Drawing.Point(41, 23);
+      this.BtnCalculate.Name = "BtnCalculate";
+      this.BtnCalculate.Size = new System.Drawing.Size(171, 72);
+      this.BtnCalculate.TabIndex = 0;
+      this.BtnCalculate.Text = "Calculate";
+      this.BtnCalculate.UseVisualStyleBackColor = true;
+      this.BtnCalculate.Click += new System.EventHandler(this.BtnCalculate_Click);
+      // 
+      // LblResult
+      // 
+      this.LblResult.AutoSize = true;
+      this.LblResult.Location = new System.Drawing.Point(57, 140);
+      this.LblResult.Name = "LblResult";
+      this.LblResult.Size = new System.Drawing.Size(93, 32);
+      this.LblResult.TabIndex = 1;
+      this.LblResult.Text = "label1";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(806, 546);
+      this.Controls.Add(this.LblResult);
+      this.Controls.Add(this.BtnCalculate);
+      this.Name = "MainFrame";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button BtnCalculate;
+    private System.Windows.Forms.Label LblResult;
+  }
+}
+
---
+++ AsyncAwait/MainFrame.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwait
+{
+  public partial class MainFrame : Form
+  {
+    public MainFrame()
+    {
+      InitializeComponent();
+    }
+
+    //public int CalculateValue()
+    //{
+    //  Thread.Sleep(5000);
+    //  return 123;
+    //}
+
+    public async Task<int> CalculateValueAsync()
+    {
+      //return Task.Factory.StartNew(() =>
+      //{
+      //  Thread.Sleep(5000);
+      //  return 123;
+      //});
+
+      await Task.Delay(5000);
+      //Thread.Sleep(5000);
+      return 123;
+    }
+
+    // ConfigureAwait()
+    // for a library, always use ConfigureAwait(false)
+
+    private async void BtnCalculate_Click(object sender, EventArgs e)
+    {
+      //int n = CalculateValue();
+      //LblResult.Text = n.ToString();
+
+      //var calculation = CalculateValueAsync();
+      //calculation.ContinueWith(t =>
+      //{
+      //  LblResult.Text = t.Result.ToString();
+      //}, TaskScheduler.FromCurrentSynchronizationContext());
+      // we want this code to look like ordinary, sync code
+
+      // notice calculation is an int
+      int value = await CalculateValueAsync();
+      //          ^^^^^ does not block; instead, everything else is a continuation
+
+      // why did this work? because we captured the synchronization context
+      // now try ConfigureAwait(false)
+
+      LblResult.Text = value.ToString();
+      await Task.Delay(5000);
+      
+      Task.Factory.StartNew(async () =>
+      {
+        
+      })
+
+      //
+      var wc = new WebClient();
+      string data = await wc.DownloadStringTaskAsync("http://google.com/robots.txt");
+      LblResult.Text = data.Split('\n')[0].Trim();
+    }
+  }
+}
